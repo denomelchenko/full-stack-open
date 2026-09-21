efb8f7824904fc55271d27eb34fc024adf3fde3d
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 11:08:56 +0200

    part4: exercise 4.22 blog list expansion step 10

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,23 +1,15 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
@@ -62,18 +54,10 @@ blogsRouter.put('/:id', async (request, response) => {
   response.json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
-
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   const blog = await Blog.findById(request.params.id)
 
-  if (blog && blog.user && blog.user.toString() !== user._id.toString()) {
+  if (blog && blog.user && blog.user.toString() !== request.user._id.toString()) {
     return response.status(401).json({ error: 'only the creator can delete a blog' })
   }
 
