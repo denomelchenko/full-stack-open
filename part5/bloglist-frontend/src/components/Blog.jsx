dd1050371e06ea71681b9fb855f125a915bf650d
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 11:31:07 +0200

    part5: exercise 5.7 blog list frontend step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle} className="blog">
+      <div className="blog-title-author">
+        <span className="blog-title">{blog.title}</span> by <span className="blog-author">{blog.author}</span>
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showWhenVisible} className="blog-details">
+        <div className="blog-url">{blog.url}</div>
+        <div className="blog-likes">
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div className="blog-user">{blog.user && blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
