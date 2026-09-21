e4cb9441262f16b305878221173124a80e844988
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 12:40:23 +0200

    part2: exercise 2.5 separate module step 10

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p>Total of {total} exercises</p>
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+export default Course
