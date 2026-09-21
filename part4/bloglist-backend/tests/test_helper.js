e5b5bf7e37374577d3ea75a9f3c753ef7212d523
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 10:57:21 +0200

    part4: exercise 4.16 blog list expansion step 4

--- part4/bloglist-backend/tests/test_helper.js
+++ part4/bloglist-backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -12,6 +13,14 @@ const initialBlogs = [
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
+  },
+]
+
+const initialUsers = [
+  {
+    username: 'root',
+    name: 'Superuser',
+    password: 'sekret',
   },
 ]
 
@@ -33,8 +42,15 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((user) => user.toJSON())
+}
+
 module.exports = {
   initialBlogs,
+  initialUsers,
   nonExistingId,
   blogsInDb,
+  usersInDb,
 }
---
+++ part4/bloglist-backend/tests/user_api.test.js
@@ -0,0 +1,132 @@
+const assert = require('node:assert')
+const { test, after, beforeEach, describe } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+const app = require('../app')
+const helper = require('./test_helper')
+const User = require('../models/user')
+
+const api = supertest(app)
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    await User.init()
+
+    const passwordHash = await bcrypt.hash(helper.initialUsers[0].password, 10)
+    const user = new User({
+      username: helper.initialUsers[0].username,
+      name: helper.initialUsers[0].name,
+      passwordHash,
+    })
+
+    await user.save()
+  })
+
+  test('users are returned as json', async () => {
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('the passwordHash of a user is never returned', async () => {
+    const response = await api.get('/api/users').expect(200)
+
+    assert.strictEqual(response.body[0].passwordHash, undefined)
+    assert.notStrictEqual(response.body[0].id, undefined)
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((user) => user.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with 400 if the username is already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+
+  test('creation fails with 400 if the username is missing', async () => {
+    const newUser = { name: 'No Username', password: 'salainen' }
+
+    const result = await api.post('/api/users').send(newUser).expect(400)
+
+    assert(result.body.error.includes('required'))
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, 1)
+  })
+
+  test('creation fails with 400 if the username is shorter than 3 characters', async () => {
+    const newUser = { username: 'ab', name: 'Short Username', password: 'salainen' }
+
+    const result = await api.post('/api/users').send(newUser).expect(400)
+
+    assert(result.body.error.includes('at least 3'))
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, 1)
+  })
+
+  test('creation fails with 400 if the password is missing', async () => {
+    const newUser = { username: 'nopassword', name: 'No Password' }
+
+    const result = await api.post('/api/users').send(newUser).expect(400)
+
+    assert(result.body.error.includes('required'))
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, 1)
+  })
+
+  test('creation fails with 400 if the password is shorter than 3 characters', async () => {
+    const newUser = { username: 'shortpassword', name: 'Short Password', password: 'ab' }
+
+    const result = await api.post('/api/users').send(newUser).expect(400)
+
+    assert(result.body.error.includes('at least 3'))
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, 1)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
