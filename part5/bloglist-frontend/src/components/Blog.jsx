5695d855162f81388bad784352ee8ea05841e32a
denomelchenko <denysomelchenko.dev@gmail.com>
2026-09-21 12:10:34 +0200

    part5: exercise 5.31 style the single blog view

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { Button, Paper, Stack, Typography } from '@mui/material'
 
 const Blog = ({ blogs, user, handleLike, handleDelete }) => {
   const id = useParams().id
@@ -14,25 +15,27 @@ const Blog = ({ blogs, user, handleLike, handleDelete }) => {
   const own = user && blog.user && user.username === blog.user.username
 
   return (
-    <div className="blog">
-      <h2>{blog.title}</h2>
-      <div>{blog.author}</div>
-      <div>
+    <Paper sx={{ p: 2, mt: 2 }}>
+      <Typography variant="h5">{blog.title}</Typography>
+      <Typography>{blog.author}</Typography>
+      <Typography>
         <a href={blog.url}>{blog.url}</a>
-      </div>
-      <div>likes {blog.likes}</div>
-      <div>{blog.user ? blog.user.name : 'unknown'}</div>
-      {user && (
-        <button type="button" onClick={() => handleLike(blog)}>
-          like
-        </button>
-      )}
-      {own && (
-        <button type="button" onClick={() => handleDelete(blog)}>
-          delete
-        </button>
-      )}
-    </div>
+      </Typography>
+      <Typography>likes {blog.likes}</Typography>
+      <Typography>{blog.user ? blog.user.name : 'unknown'}</Typography>
+      <Stack direction="row" spacing={1} sx={{ mt: 1 }}>
+        {user && (
+          <Button variant="contained" color="primary" onClick={() => handleLike(blog)}>
+            like
+          </Button>
+        )}
+        {own && (
+          <Button variant="outlined" color="error" onClick={() => handleDelete(blog)}>
+            delete
+          </Button>
+        )}
+      </Stack>
+    </Paper>
   )
 }
 
